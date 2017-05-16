62ac41226552aa937c10e220b42c38042f1b7a59
Mimba <baounabaouna@gmail.com>
2017-05-16 21:23:56 +0100

    Initialisation

---
+++ src/main/java/org/mimba/bao/CliniqueMedicaleAcceuilServiceApplication.java
@@ -0,0 +1,12 @@
+package org.mimba.bao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CliniqueMedicaleAcceuilServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CliniqueMedicaleAcceuilServiceApplication.class, args);
+	}
+}
